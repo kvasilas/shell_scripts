046c2544dceeea5f8cc0b4ab5827376c26f8bafa
KVasilas <kcvasilas@gmail.com>
2019-03-15 13:10:16 -0400

    needs address

--- term_boot.sh
+++ term_boot.sh
@@ -1,10 +1,25 @@
+#!/bin/sh
+#
+# Written By Kirk Vasilas
+#
+# Script to display fortune and pull class_work repo on terminal bootup
+# Linnux 
+
 figlet HELLO  KC
 
 fortune | cowsay | lolcat
 
-echo Today is: 
+echo Today is:
 date
 
-echo 
+echo
 echo Starting Directory is:
 pwd
+
+REPO_CLASS='address'
+cd $REPO_CLASS
+echo "Pulling Class Work Repo"
+git pull
+cd ~
+
+exit 0

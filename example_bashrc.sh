d2110c2d49c5bcdc007df4394bd46ebe9548c725
Kirk Vasilas <35264676+kvasilas@users.noreply.github.com>
2019-07-16 10:08:44 -0400

    Update example_bashrc.sh

--- example_bashrc.sh
+++ example_bashrc.sh
@@ -3,3 +3,7 @@ alias go_here="cd /folder/folder/place"
 function do_this () {
     sudo python3 /path/filename.py
 }
+
+alias run_media="python3 /home/greenport/documents/RPI_home_theater/rpi_gui.py"
+
+alias cdr="cd /home/greenport/documents/RPI_home_theater/"

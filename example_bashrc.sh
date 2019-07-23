f16fd16f84ec8752f59ad10a9ca85cee2b0a1839
Kirk Vasilas <35264676+kvasilas@users.noreply.github.com>
2019-07-23 11:53:40 -0400

    Update example_bashrc.sh

--- example_bashrc.sh
+++ example_bashrc.sh
@@ -4,6 +4,6 @@ function do_this () {
     sudo python3 /path/filename.py
 }
 
-alias run_media="python3 /home/greenport/documents/RPI_home_theater/rpi_gui.py"
+alias run_media="bash media_startup.sh"
 
 alias cdr="cd /home/greenport/documents/RPI_home_theater/"

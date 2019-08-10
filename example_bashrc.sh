f3dfe793ab21b6bbe12146633e91ab54893ad8eb
KVasilas <kcvasilas@gmail.com>
2019-08-10 00:59:29 -0400

    clean

--- example_bashrc.sh
+++
@@ -1,9 +0,0 @@
-alias go_here="cd /folder/folder/place"
-
-function do_this () {
-    sudo python3 /path/filename.py
-}
-
-alias run_media="bash media_startup.sh"
-
-alias cdr="cd /home/greenport/documents/RPI_home_theater/"
